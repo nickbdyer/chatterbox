d99d9454d85420fddacb37663a4f07f31190d548
Nick Dyer <nbdyer@gmail.com>
2014-10-25 16:43:51 +0100

    Add quit option

--- chatbot.rb
+++ chatbot.rb
@@ -9,18 +9,23 @@ RESPONSES = { 'goodbye' => 'bye',
               'sayonara' => 'sayonara', 
               'the weather is (.*)' => 'I hate it when it\'s %{c1}', 
               'I love (.*)' => 'I love %{c1} too', 
-              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}'
+              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}',
           	  'Coding is (.*)' => 'I agree coding is %{c1}',
           	  'I enjoy learning new (.*) in ruby' => 'Learning new %{c1} in ruby is great!',
           	  'I start at Makers on (.*)' => 'No way, so do I!',
           	  'What are you doing tomorrow, I\'m (.*) ' => 'I\'m %{c1} too',
           	  'I learnt some thing about (.*) today!' => 'I already know about %{c1}', 
           	  'I have added (.*) more capture groups!' => 'What are you going to do with %{c1} more capture groups?',
-          	  'For lunch I am having a salad with (.*), (.*), (.*), (.*) and (.*)' => 'I like %{c2} and %{c3}, but hate %{c1}, %{c4} and %{c5}!' }
+          	  'For lunch I am having a salad with (.*), (.*), (.*), (.*) and (.*)' => 'I like %{c2} and %{c3}, but hate %{c1}, %{c4} and %{c5}!',
+          	   }
 
 puts "Hello, what's your name?"
 name = gets.chomp
 puts "Hello #{name}"
 while(input = gets.chomp) do
-  puts get_response(input)
+  if input == "quit" 
+  	return exit
+  else
+  	puts get_response(input)
+  end
 end
