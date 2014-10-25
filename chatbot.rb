6111eb8cdc8f8c1be155734d029e0ee0a49d3808
Nick Dyer <nbdyer@gmail.com>
2014-10-25 14:17:50 +0100

    Add more responses

--- chatbot.rb
+++ chatbot.rb
@@ -9,7 +9,12 @@ RESPONSES = { 'goodbye' => 'bye',
               'sayonara' => 'sayonara', 
               'the weather is (.*)' => 'I hate it when it\'s %{c1}', 
               'I love (.*)' => 'I love %{c1} too', 
-              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}'}
+              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}'
+          	  'Coding is (.*)' => 'I agree coding is %{c1}',
+          	  'I enjoy learning new (.*) in ruby' => 'Learning new %{c1} in ruby is great!',
+          	  'I start at Makers on (.*)' => 'No way, so do I!',
+          	  'What are you doing tomorrow, I\'m (.*) ' => 'I\'m %{c1} too',
+          	  'I learnt some thing about (.*) today!' => 'I already know about %{c1}' }
 
 puts "Hello, what's your name?"
 name = gets.chomp
